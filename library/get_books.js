dbbff85348fae8b1366a066248ef41c857290b3f
W Brian Leonard <brianleonard@yahoo.com>
2017-09-25 18:50:10 -0400

    Added formatting for Facebook messenger

--- library/get_books.js
+++ library/get_books.js
@@ -27,6 +27,7 @@ module.exports = {
         var subject = conversation.text();
         var ccPhase = conversation.variable("bookSelectionPhase");
         var baseURL = "http://140.86.40.251:8080/instructional/instructors/disciplines/" + subject;
+        var fbmList = "";
         console.log("Selected subject = " + subject);
 
         // ====================================================================
@@ -43,6 +44,15 @@ module.exports = {
                 console.log('Headers:', JSON.stringify(response.headers));
                 console.log('Response:', body);
 
+                // Format reply for Facebook
+                // https://developers.facebook.com/docs/messenger-platform/send-messages/template/list
+                // To troubleshoot, docker ps the bots-connectors container.
+                if (conversation.channelType() == "facebook") {
+                    fbmList = "{ \"attachment\":{ \"type\":\"template\", " +
+                        "\"payload\":{ \"template_type\":\"list\",\"top_element_style\":\"compact\", " +
+                        "\"elements\":[";
+                }
+
                 var jsonBody = JSON.parse(body);
 
                 var recordsFound = Number(jsonBody.recordsFound)
@@ -56,39 +66,55 @@ module.exports = {
                     conversation.keepTurn(true);
                     conversation.transition("selectAnotherBook");
                     done();
-                    console.log("*** Exiting invoke get_subjects ***\n");    
+                    console.log("*** Exiting invoke get_subjects ***\n");
 
                 } else {
 
                     // Build the choices array from the response body...
                     var choices = [];
                     for (var i = 0; i < jsonBody.results.length; i++) {
-                        choices.push(jsonBody.results[i].title);
-                        console.log(jsonBody.results[i].title);
+                        if (conversation.channelType() == 'facebook') {
+                            if (i < 3) { // The Facebook list can't handle more then 4 elements
+                                fbmList += "{\"title\":\"" + jsonBody.results[i].title + "\"," +
+                                    "\"subtitle\":\"" + jsonBody.results[i].authors + "\"," +
+                                    "\"buttons\":[{ \"type\":\"postback\", \"title\":\"Buy\", \"payload\":\"" + jsonBody.results[i].title + "\"}]" +
+                                    "},";
+                            }
+                        } else {
+                            choices.push(jsonBody.results[i].title);
+                            console.log(jsonBody.results[i].title);
+                        }
                     }
 
-                    conversation.reply({
-                        text: "[Dynamic] Here are the available " + subject + " books:",
-                        choices: choices    
-                    });
+                    if (conversation.channelType() == 'facebook') {
+                        fbmList = fbmList.substr(0, (fbmList.length - 1)); // Strip trailing comma
+                        fbmList += "] } } }";
+                        console.log(fbmList);
+                        conversation.reply(JSON.parse(fbmList));
+                    } else {
+                        conversation.reply({
+                            text: "[Dynamic] Here are the available " + subject + " books:",
+                            choices: choices
+                        });
+                    }
 
                     conversation.keepTurn(false); // to keep the control of user input            
                     conversation.variable("bookSelectionPhase", 2);
                     done();
-                    console.log("*** Exiting invoke get_subjects ***\n");                        
+                    console.log("*** Exiting invoke get_subjects ***\n");
                 }
             });
         } else {
 
             // Second call to CC so User is selecting a book ==================
-            console.log ("Entering get_books phase 2...");
+            console.log("Entering get_books phase 2...");
             var selectedBook = conversation.text();
-            console.log ("selectedBook = " + selectedBook);
+            console.log("selectedBook = " + selectedBook);
             conversation.variable("selectedBook", selectedBook);
             conversation.keepTurn(true);
             conversation.transition("confirmBookSelection");
             done();
-            console.log("*** Exiting invoke get_subjects ***\n");                
+            console.log("*** Exiting invoke get_subjects ***\n");
         }
     }
 };
--- library/get_subjects.js
+++ library/get_subjects.js
@@ -19,6 +19,7 @@ module.exports = {
 
         // CC specific variables ==============================================
         var baseURL = "http://140.86.40.251:8080/instructional/instructors/disciplines/";
+        var fbmList = "";
 
         console.log(conversation.text());
 
@@ -31,23 +32,48 @@ module.exports = {
             console.log('Headers:', JSON.stringify(response.headers));
             console.log('Response:', body);
 
+            // Format reply for Facebook
+            // https://developers.facebook.com/docs/messenger-platform/send-messages/template/list
+            // To troubleshoot, docker ps the bots-connectors container.
+            if (conversation.channelType() == "facebook") {
+                fbmList = "{ \"attachment\":{ \"type\":\"template\", " +
+                    "\"payload\":{ \"template_type\":\"list\",\"top_element_style\":\"compact\", " +
+                    "\"elements\":[";
+            }
+
             // Build the choices array from the response body...
             var choices = [];
             var jsondata = JSON.parse(body);
-            for (var i=0; i<jsondata.length; i++){
-                choices.push(jsondata[i]);
-                //console.log(jsondata[i]);    
+            for (var i = 0; i < jsondata.length; i++) {
+                if (conversation.channelType() == 'facebook') {
+                    if (i < 3) { // The Facebook list can't handle more then 4 elements
+                        fbmList += "{\"title\":\"" + jsondata[i] + "\"," +
+                                   //"\"subtitle\":\"" + jsondata[i] + "\"," +
+                                   "\"buttons\":[{ \"type\":\"postback\", \"title\":\"Select\", \"payload\":\"" + jsondata[i] + "\"}]" +
+                            "},";
+                    }
+                } else {
+                    choices.push(jsondata[i]);
+                    //console.log(jsondata[i]);    
+                }
             }
-            
-            conversation.reply({
-                text: "For which subject do you need a book?",
-                //choices: ["Departmental Math", "Precalculus", "Advanced Math", "Statistics", "Physics", "Chemistry", "Biology", "Astronomy"]
-                choices: choices
-            });
+
+            if (conversation.channelType() == 'facebook') {
+                fbmList = fbmList.substr(0, (fbmList.length - 1)); // Strip trailing comma
+                fbmList += "] } } }";
+                console.log(fbmList);
+                conversation.reply(JSON.parse(fbmList));
+            } else {
+                conversation.reply({
+                    text: "For which subject do you need a book?",
+                    //choices: ["Departmental Math", "Precalculus", "Advanced Math", "Statistics", "Physics", "Chemistry", "Biology", "Astronomy"]
+                    choices: choices
+                });
+            }
 
             conversation.transition(true);
             done();
-            console.log("*** Exiting invoke get_subjects ***\n");            
+            console.log("*** Exiting invoke get_subjects ***\n");
         });
     }
 };
